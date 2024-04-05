75880005ed6e41932b88ccbe06b0cfa5f0c96224
Jordi <jordi.robles.perera@gmail.com>
2024-04-05 15:56:00 +0200

    Carpeta Dao feta

--- src/main/kotlin/com/example/dao/DAOCites.kt
+++ src/main/kotlin/com/example/dao/DAOCites.kt
@@ -1,4 +1,10 @@
 package com.example.dao
 
+import com.example.model.Cita
+
 interface DAOCites {
+    suspend fun allCites():List<Cita>
+    suspend fun cites(cites_id: Int): Cita?
+    suspend fun citesNom (cites_nom:String): Cita?
+    suspend fun addNewcites(cites_nom: String, cites_comentaris:String): Cita?
 }
