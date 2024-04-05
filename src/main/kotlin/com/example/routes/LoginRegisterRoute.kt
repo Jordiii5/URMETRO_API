39a5c5b87b83dbde5ec5da340cc42385c51fc856
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2024-04-05 16:03:12 +0200

    Començament rutes

--- src/main/kotlin/com/example/routes/LoginRegisterRoute.kt
+++ src/main/kotlin/com/example/routes/LoginRegisterRoute.kt
@@ -1,3 +1,10 @@
 package com.example.routes.sdsd
 
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
+
+fun Routing.loginRegisterRouting() {
+
+}
