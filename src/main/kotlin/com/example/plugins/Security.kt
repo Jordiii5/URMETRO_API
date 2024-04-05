bc4c6088a5ea68848070af410d77a146f6c202df
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2024-04-05 16:57:23 +0200

    Intent de login i register 2.0

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -1,6 +1,19 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 
 fun Application.configureSecurity() {
+    install(Authentication) {
+        basic("myAuth") {
+            realm = "Ktor Server"
+            validate { credentials ->
+                if (credentials.name == "admin" && credentials.password == "admin") {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+        }
+    }
 }
