fc5b1a165bb88643245258dba6e0ba2dce305f6c
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2024-04-05 16:51:12 +0200

    Intent de login i register

--- src/main/kotlin/com/example/routes/LoginRegisterRoute.kt
+++ src/main/kotlin/com/example/routes/LoginRegisterRoute.kt
@@ -1,10 +1,39 @@
 package com.example.routes.sdsd
 
+import com.example.dao.daoUsuario
+import com.example.model.*
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 
 fun Routing.loginRegisterRouting() {
-
+    route("user") {
+        post("/register") {
+            val user = call.receive<Usuari>()
+            uploadUser()
+            if (userTable.contains(user.usuari_nom)) {
+                call.respondText("Aquest usuari ja existeix", status = HttpStatusCode.Conflict)
+                return@post
+            } else {
+                userTable[user.usuari_nom] = getMd5Digest("${user.usuari_nom}:$myRealm:${user.usuari_contra}")
+                daoUsuario.addNewUsuario(user.usuari_nom, user.usuari_contra)
+                userTable.put(user.usuari_nom, getMd5Digest("${user.usuari_nom}:$myRealm:${user.usuari_contra}"))
+                call.respondText("Usuari registrat amb exit", status = HttpStatusCode.Created)
+            }
+        }
+        post("/login") {
+            val user = call.receive<Usuari>()
+            userTable = uploadUser()
+            val userHidden = getMd5Digest("${user.usuari_nom}:$myRealm:${user.usuari_contra}")
+            if (userTable.contains(user.usuari_nom) && userTable[user.usuari_nom]?.contentEquals(userHidden) == true) {
+                call.respondText("Usuari autenticat", status = HttpStatusCode.OK)
+                return@post
+            } else {
+                call.respondText("Usuari o contrasenya incorrectes", status = HttpStatusCode.Unauthorized)
+            }
+        }
+    }
 }
