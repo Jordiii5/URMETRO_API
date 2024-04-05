4a2d2581e9817f67fe461aa3d65e0fdd15559c22
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2024-04-05 16:03:12 +0200

    Començament rutes

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,12 @@
 package com.example.plugins
 
+import com.example.routes.sdsd.loginRegisterRouting
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Helloooo World!")
-        }
+        loginRegisterRouting()
     }
 }
