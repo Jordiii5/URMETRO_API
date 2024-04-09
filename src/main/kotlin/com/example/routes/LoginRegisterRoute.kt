5374beebee2b35f3fd58a05112abe0bc7d43b321
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2024-04-09 16:36:59 +0200

    rutas login y registro acabadas

--- src/main/kotlin/com/example/routes/LoginRegisterRoute.kt
+++ src/main/kotlin/com/example/routes/LoginRegisterRoute.kt
@@ -14,13 +14,13 @@ fun Routing.loginRegisterRouting() {
         post("/register") {
             val user = call.receive<Usuari>()
             uploadUser()
-            if (userTable.contains(user.usuari_nom)) {
+            if (userTable.contains(user.usuari_dni)) {
                 call.respondText("Aquest usuari ja existeix", status = HttpStatusCode.Conflict)
                 return@post
             } else {
-                userTable[user.usuari_nom] = getMd5Digest("${user.usuari_nom}:$myRealm:${user.usuari_contra}")
+                userTable[user.usuari_dni] = getMd5Digest("${user.usuari_dni}:$myRealm:${user.usuari_contra}")
                 daoUsuario.addNewUsuario(user.usuari_nom, user.usuari_dni, user.usuari_adreça, user.usuari_telefon, user.usuari_contacte_emergencia, user.usuari_imatge, user.usuari_contra)
-                userTable.put(user.usuari_nom, getMd5Digest("${user.usuari_nom}:$myRealm:${user.usuari_contra}"))
+                userTable.put(user.usuari_dni, getMd5Digest("${user.usuari_dni}:$myRealm:${user.usuari_contra}"))
                 call.respondText("Usuari registrat amb exit", status = HttpStatusCode.Created)
             }
         }
--- src/main/kotlin/com/example/routes/UsuariRoute.kt
+++ src/main/kotlin/com/example/routes/UsuariRoute.kt
@@ -11,96 +11,74 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.SerializationException
 
 fun Routing.usuariRouting() {
-    authenticate("myAuth") {
-        route("/usuaris") {
-           get {
-               val llistaUsuari = daoUsuario.allUsuaris()
-                if (llistaUsuari.isEmpty()) {
-                     call.respondText("No hi ha usuaris", status = HttpStatusCode.NotFound)
-                } else {
-                     call.respond(llistaUsuari)
+    route("/usuaris") {
+        get {
+            val llistaUsuari = daoUsuario.allUsuaris()
+            if (llistaUsuari.isEmpty()) {
+                call.respondText("No hi ha usuaris", status = HttpStatusCode.NotFound)
+            } else {
+                call.respond(llistaUsuari)
+            }
+        }
+        get("/{id}") {
+            val id = call.parameters["id"]?: return@get call.respondText(
+                "No existeix cap usuari amb la id proporcionada",
+                status = HttpStatusCode.BadRequest
+            )
+            val usuari = daoUsuario.usuario(id.toInt())?: return@get call.respondText(
+                "No existeix cap usuari amb la id $id proporcionada",
+                status = HttpStatusCode.NotFound
+            )
+            call.respond(usuari)
+        }
+        post {
+            val usuari = call.receive<Usuari>()
+            val usuariCreat = daoUsuario.addNewUsuario(usuari.usuari_nom, usuari.usuari_dni, usuari.usuari_adreça, usuari.usuari_telefon, usuari.usuari_contacte_emergencia, usuari.usuari_imatge, usuari.usuari_contra)
+
+            if (usuariCreat == null) {
+                call.respondText("No s'ha pogut crear l'usuari", status = HttpStatusCode.BadRequest)
+            } else {
+                call.respond(usuariCreat)
+            }
+        }
+
+        delete("/{id}") {
+            val id = call.parameters["id"]?: return@delete call.respond(HttpStatusCode.BadRequest)
+            val usuari = daoUsuario.usuario(id.toInt())?: return@delete call.respondText (
+                "No existeix cap usuari amb la id $id proporcionada",
+                status = HttpStatusCode.NotFound
+            )
+            if (daoUsuario.deleteUsuario(id.toInt())) {
+                call.respondText("Usuari eliminat correctament", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("No s'ha pogut eliminar l'usuari", status = HttpStatusCode.BadRequest)
+            }
+        }
+
+        put("/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid Id")
+            } else {
+                val usuari = try {
+                    call.receive<Usuari>()
+                } catch (e: SerializationException) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid data")
+                    return@put
                 }
-           }
-            get("({id})") {
-                val id = call.parameters["id"]?: return@get call.respondText(
-                    "No existeix cap usuari amb la id proporcionada",
-                    status = HttpStatusCode.BadRequest
+                val updatedPost = daoUsuario.updateUsuario(
+                    usu_id = usuari.usuari_id,
+                    usu_nom = usuari.usuari_nom,
+                    usu_dni = usuari.usuari_dni,
+                    usu_adreça = usuari.usuari_adreça,
+                    usu_telefon = usuari.usuari_telefon,
+                    usu_contacte_emergencia = usuari.usuari_contacte_emergencia,
+                    usu_imatge = usuari.usuari_imatge
                 )
-                val usuari = daoUsuario.usuario(id.toInt())?: return@get call.respondText(
-                    "No existeix cap usuari amb la id $id proporcionada",
-                    status = HttpStatusCode.NotFound
-                )
-                call.respond(usuari)
-            }
-            post {
-                val usuari = call.receive<Usuari>()
-                val usuariCreat = daoUsuario.addNewUsuario(usuari.usuari_nom, usuari.usuari_dni, usuari.usuari_adreça, usuari.usuari_telefon, usuari.usuari_contacte_emergencia, usuari.usuari_imatge, usuari.usuari_contra)
-
-                if (usuariCreat == null) {
-                    call.respondText("No s'ha pogut crear l'usuari", status = HttpStatusCode.BadRequest)
+                if (updatedPost) {
+                    call.respond(HttpStatusCode.OK, "Usuari actualitzat correctament")
                 } else {
-                    call.respond(usuariCreat)
-                }
-            }
-
-            delete("({id})") {
-                val id = call.parameters["id"]?: return@delete call.respond(HttpStatusCode.BadRequest)
-                val usuari = daoUsuario.usuario(id.toInt())?: return@delete call.respondText (
-                    "No existeix cap usuari amb la id $id proporcionada",
-                    status = HttpStatusCode.NotFound
-                )
-                if (daoUsuario.deleteUsuario(id.toInt())) {
-                    call.respondText("Usuari eliminat correctament", status = HttpStatusCode.Accepted)
-                } else {
-                    call.respondText("No s'ha pogut eliminar l'usuari", status = HttpStatusCode.BadRequest)
-                }
-            }
-
-            put("/{id}") {
-                val id = call.parameters["id"]?.toIntOrNull()
-                if (id == null) {
-                    call.respond(HttpStatusCode.BadRequest, "Invalid Id")
-                } else {
-                    val usuari = try {
-                        call.receive<Usuari>()
-                    } catch (e: SerializationException) {
-                        call.respond(HttpStatusCode.BadRequest, "Invalid data")
-                        return@put
-                    }
-                    val updatedPost = daoUsuario.updateUsuario(
-                        usu_id = usuari.usuari_id,
-                        usu_nom = usuari.usuari_nom,
-                        usu_dni = usuari.usuari_dni,
-                        usu_adreça = usuari.usuari_adreça,
-                        usu_telefon = usuari.usuari_telefon,
-                        usu_contacte_emergencia = usuari.usuari_contacte_emergencia,
-                        usu_imatge = usuari.usuari_imatge
-                    )
-                    if (updatedPost) {
-                        call.respond(HttpStatusCode.OK, "Usuari actualitzat correctament")
-                    } else {
-                        call.respond(HttpStatusCode.InternalServerError, "Problemes per actualitzar l'usuari")
-                    }
-                }
-            }
-
-            delete("/{id}") {
-                val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
-                daoUsuario.deleteUsuario(id.toInt())
-                call.respondText("Usuari eliminat", status = HttpStatusCode.Accepted)
-            }
-
-            delete("/{id}") {
-                val id = call.parameters["id"]?.toIntOrNull()
-                if (id == null) {
-                    call.respond(HttpStatusCode.BadRequest, "Invalid Id")
-                } else {
-                    val deletedUser = daoUsuario.deleteUsuario(id)
-                    if (deletedUser) {
-                        call.respond(HttpStatusCode.OK, "Usuario eliminat correctament")
-                    } else {
-                        call.respond(HttpStatusCode.InternalServerError, "Problemes al eliminar l'usuari")
-                    }
+                    call.respond(HttpStatusCode.InternalServerError, "Problemes per actualitzar l'usuari")
                 }
             }
         }
